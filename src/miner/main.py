8a0f06b9b957e91c30b608f12fa7909a196a8831
Joemen <b01902103@ntu.edu.tw>
2016-06-16 01:26:52 +0800

    First try

--- src/miner/main.py
+++ src/miner/main.py
@@ -6,6 +6,7 @@ import sys
 
 from modules.backend_interface import BackendInterface
 from modules.server import Server
+from modules.real_main import BBSCrawler
 
 
 def main():
@@ -18,10 +19,21 @@ def main():
                     help='Address the server should bind to.')
     ap.add_argument('--server_port', type=int, nargs='?', default=8993,
                     help='The port the server listen to.')
-
+    ap.add_argument('--board', type=str, nargs='?', default='Gossiping',
+                    help='The Board you want to mine.')
+    ap.add_argument('--number', type=int, nargs='?', default=2,
+                    help='The Pages you want to mine.')
+    ap.add_argument('--fetch_path', type=str, nargs='?', default='./',
+                    help='The Place you want to put your mining file')
+    ap.add_argument('--mining', type=bool, nargs='?', default=False,
+                    help='Mining or not')
     opts = ap.parse_args()
 
     # TODO(joemen): Change it to the real backend server.
+    if opts.mining == True:
+        miner = BBSCrawler(opts.board,opts.number,opts.fetch_path)
+        miner.getAllPagesInTheBoard()
+        miner.getContent()
     backend = BackendInterface()
     server = Server(backend, opts.server_addr, opts.server_port)
     try:
